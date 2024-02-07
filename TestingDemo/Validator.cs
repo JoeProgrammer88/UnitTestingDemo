0af4351e9fa981340bf36331cb4a50e98428470a
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2024-02-07 13:29:28 -0800

    Add project files.

---
+++ TestingDemo/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDemo
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Checks to see if the numToCheck is within the range of min and max
+        /// inclusive
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="numToCheck"></param>
+        /// <returns></returns>
+        public static bool IsWithinRange(double min, double max, double numToCheck)
+        {
+            if (numToCheck >= min &&  numToCheck <= max) 
+                return true;
+
+            return true;
+        }
+    }
+}
---
+++ TestingDemoTests/ValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDemo.Tests
+{
+    [TestClass()]
+    public class ValidatorTests
+    {
+        [TestMethod]
+        public void Test_IsWithinRange_EqualToMin_ReturnsTrue()
+        {
+            // Arrange
+            double min = 1.0;
+            double max = 10.0;
+            double numToCheck = min;
+
+            // Act
+            bool result = Validator.IsWithinRange(min, max, numToCheck);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_IsWithinRange_EqualToMax_ReturnsTrue()
+        {
+            // Arrange
+            double min = 1.0;
+            double max = 10.0;
+            double numToCheck = max;
+
+            // Act
+            bool result = Validator.IsWithinRange(min, max, numToCheck);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_IsWithinRange_BetweenBoundaries_ReturnsTrue()
+        {
+            // Arrange
+            double min = 1.0;
+            double max = 10.0;
+            double numToCheck = 5.0;
+
+            // Act
+            bool result = Validator.IsWithinRange(min, max, numToCheck);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
